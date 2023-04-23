1051046192dd4ea68b2bbff88118a76a67cf3a95
harvey186 <harvey186@hotmail.com>
2023-04-23 16:30:07 +0200

    package name and folder renamed

--- app/src/main/kotlin/com/goodwy/contacts/interfaces/RefreshContactsListener.kt
+++
@@ -1,9 +0,0 @@
-package com.goodwy.contacts.interfaces
-
-import com.goodwy.contacts.models.Contact
-
-interface RefreshContactsListener {
-    fun refreshContacts(refreshTabsMask: Int)
-
-    fun contactClicked(contact: Contact)
-}
--- app/src/main/kotlin/com/goodwy/contacts/interfaces/RemoveFromGroupListener.kt
+++
@@ -1,7 +0,0 @@
-package com.goodwy.contacts.interfaces
-
-import com.goodwy.contacts.models.Contact
-
-interface RemoveFromGroupListener {
-    fun removeFromGroup(contacts: ArrayList<Contact>)
-}
